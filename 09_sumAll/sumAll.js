2c5ce49358ea61f2028fe179b0ea0b980ca62502
kimbaesung808 <kimbaesung808@gmail.com>
2025-05-12 07:25:49 -1000

    Implement `sumAll`

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  } else if (a % 1 !== 0 || b % 1 !== 0) {
+    return "ERROR";
+  } else if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  let i;
+  let j;
+
+  if (a < b) {
+    i = a;
+    sum = i++;
+    j = b;
+  } else {
+    i = b;
+    sum = i++;
+    j = a;
+  }
+
+  for (; i <= j; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
