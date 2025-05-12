3b434acf10cffa9b854a0726c54b833c7fce40a7
kimbaesung808 <kimbaesung808@gmail.com>
2025-05-12 06:57:57 -1000

    Implement `removeFromArray`

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(nums, ...numsToRemove) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < numsToRemove.length; j++) {
+      if (nums[i] === numsToRemove[j]) {
+        nums.splice(i--, 1);
+      }
+    }
+  }
+
+  return nums;
 };
+
+console.log(removeFromArray([ 1, 2, 3, 4 ], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
